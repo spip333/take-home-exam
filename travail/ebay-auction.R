234bec0b9786fc1f365394d84cd38341fd025835
(U115136) <nicolas.stern@mobi.ch>
2019-01-03 13:00:07 +0100

    resolution auktion - wip

---
+++ travail/ebay-auction.R
@@ -0,0 +1,25 @@
+##########################################################
+# ebay -auction	
+##########################################################
+
+
+library(dplyr)
+library(foreign)
+
+#========================================================
+# init environment
+rm(list = ls())
+setwd("C:/ieu/workspace/R/tooling-und-datenmanagement/take-home-exam")
+
+
+#========================================================
+# load data
+ebay <- read.dta("http://www.farys.org/daten/ebay.dta")
+
+# filter out rows where sepos < 12, add a column "rating" and a column "makellos"
+ebaynew <- ebay %>%
+  filter(sepos >= 12) %>%
+  mutate(
+    rating =  sepos / (seneg + sepos),
+    makellos = rating > 0.98
+  )
