e6e70de8143727696e9b78c73ed503c6275269a3
(U115136) <nicolas.stern@mobi.ch>
2019-01-06 12:31:35 +0100

    introduced file local saving

--- recherche/recherche-webscrapping.R
+++ recherche/recherche-webscrapping.R
@@ -8,3 +8,10 @@ library(foreign)
 #========================================================
 # load data from https://de.wikipedia.org/wiki/Bern#Klima
 
+Sys.Date()
+
+today <- Sys.Date()
+filename <- paste("ebay-", today, ".dta", sep="")
+
+today + "test"
+append("a", "b")
