825dbb8a17d439fbb005ceeebab9d1fa6556ac0e
(U115136) <nicolas.stern@mobi.ch>
2019-01-06 17:59:00 +0100

    aufgabe 2 finished

--- recherche/webscrapping.R
+++ recherche/webscrapping.R
@@ -14,6 +14,8 @@ url <- "https://de.wikipedia.org/wiki/Bern#Klima"
 bern.klima <- read_html('bern-klima.html')
 klima.data <- html_table(bern.klima,fill=TRUE,header=TRUE)[[6]]
 
+str(bern.klima)
+
 #========================================================
 # task 1 : only 3 first lines
 klima.data.1 <- klima.data[1:2,]
@@ -26,9 +28,10 @@ klima.data.2 <- t(klima.data.1)
 klima.data.3 <- klima.data.2[-c(1,14,15),]
 
 klima.data.3
+rnum(klima.data.3)
 
-for (i in 1:12){
-  for (j in 1:2){
+for (i in 1:nrow(klima.data.3)){
+  for (j in 1:ncol(klima.data.3)){
     klima.data.3[i,j] <- str_replace(klima.data.3[i,j], ",", ".")
   }
 }
@@ -55,3 +58,8 @@ klima.data.df
 
 
 kable(klima.data.df)
+
+d <- Sys.Date()
+
+format(d, format="%d.%m.%Y")
+"
